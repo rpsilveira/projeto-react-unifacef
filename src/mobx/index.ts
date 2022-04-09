553af8be0c7908b886f4d5b67f3b9872cb497850
Reinaldo Silveira <reinaldopsilveira@gmail.com>
2022-04-09 16:45:06 -0300

    urna eletronica

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -5,6 +5,7 @@ import { starWars } from '../containers/star-wars/store';
 import { starWarsDetails } from '../containers/star-wars-details/store';
 import { register } from '../containers/register/store';
 import { cache } from '../containers/cache/store';
+import { eleicoes } from '../containers/eleicoes/store';
 
 export {
   router,
@@ -14,4 +15,5 @@ export {
   starWarsDetails,
   register,
   cache,
+  eleicoes,
 }
--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -5,6 +5,7 @@ import StarWars from "../containers/star-wars";
 import StarWarsDetails from "../containers/star-wars-details";
 import Register from '../containers/register';
 import Cache from "../containers/cache";
+import Eleicoes from "../containers/eleicoes";
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -20,4 +21,5 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true },
   { path: `${publicUrl}/cep`, name: 'Register', component: Register, exact: true },
   { path: `${publicUrl}/cache`, name: 'Cache', component: Cache, exact: true },
+  { path: `${publicUrl}/eleicoes`, name: 'Eleições', component: Eleicoes, exact: true },
 ];
